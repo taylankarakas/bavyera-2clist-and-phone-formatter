76927d8569c172d81aa5523b6a36c72b896a4739
Atıl Taylan Karakaş <taylan@hub.studio>
2022-01-09 23:19:59 +0300

    updated modules export type

--- index.js
+++ index.js
@@ -8,55 +8,57 @@ const countryCodes = require('./countryCodes.js');
 const pkg = require('./package.json');
 updateNotifier({pkg}).notify();
 
-module.exports = {
-    countryCodeList: () => {
-        try {
-            const sortedListByPhone = _.sortBy(countryCodes, ['phone']);
-            const output = _.map(sortedListByPhone, ({ phone, emoji, code }, key) => ({
-                phone,
-                emoji,
-                code,
-                key
-            }))
+countryCodeList = () => {
+    try {
+        const sortedListByPhone = _.sortBy(countryCodes, ['phone']);
+        const output = _.map(sortedListByPhone, ({ phone, emoji, code }, key) => ({
+            phone,
+            emoji,
+            code,
+            key
+        }))
 
-            return output;
-            
-        } catch (e) {
-            return { phone: '', emoji: '', code: '', key: ''}
+        return output;
+        
+    } catch (e) {
+        return { phone: '', emoji: '', code: '', key: ''}
+    }
+};
+
+countryCodeFilteredList = (filterKey) => {
+    try {
+        const key = parseInt(filterKey);
+        if (isNaN(key)) {
+            const upperCaseKey = filterKey.toUpperCase();
+            return _.find(countryCodeList(), ['code', upperCaseKey]);
         }
-    },
+        return _.find(countryCodeList(), ['phone', key]);
 
-    countryCodeFilteredList: (filterKey) => {
-        try {
-            const key = parseInt(filterKey);
-            if (isNaN(key)) {
-                const upperCaseKey = filterKey.toUpperCase();
-                return _.find(this.countryCodeList(), ['code', upperCaseKey]);
-            }
-            return _.find(this.countryCodeList(), ['phone', key]);
+    } catch (e) {
+        console.log(e)
+        return {};
+    }
+},
 
-        } catch (e) {
-            return {};
-        }
-    },
+phoneNumberValidation = (number, countryCode) => {
+    try {
+        const country = countryCodeFilteredList(countryCode)
+        const phoneNumber = phoneUtil.parse(number.split('+').join(''), country['code']);
 
-    phoneNumberValidation: (number, countryCode) => {
-        try {
-            const country = this.countryCodeFilteredList(countryCode)
-            const phoneNumber = phoneUtil.parse(number.split('+').join(''), country['code']);
+        return {
+            phone : phoneUtil.format(phoneNumber, PNF.E164),
+            isValid : phoneUtil.isValidNumber(phoneNumber),
+            success: true,
+            code: country['code'],
+            flag: country['emoji']
+          }
 
-            return {
-                phone : phoneUtil.format(phoneNumber, PNF.E164),
-                isValid : phoneUtil.isValidNumber(phoneNumber),
-                success: true,
-                code: country['code'],
-                flag: country['emoji']
-              }
-
-        } catch (e) {
-            return {
-                success: false,
-            }
+    } catch (e) {
+        console.log(e)
+        return {
+            success: false,
         }
     }
 }
+
+module.exports = { countryCodeList, countryCodeFilteredList, phoneNumberValidation };
