e62ea8e4048b628dc77144e981295e2684fd8b61
Atıl Taylan Karakaş <taylan@hub.studio>
2022-01-09 22:00:13 +0300

    first commit

---
+++ countryCodes.js
@@ -0,0 +1,1253 @@
+module.exports = {
+   AD: {
+     phone: 376,
+     emoji: '🇦🇩',
+     code: 'AD',
+   },
+   AE: {
+     phone: 971,
+     emoji: '🇦🇪',
+     code: 'AE',
+   },
+   AF: {
+     phone: 93,
+     emoji: '🇦🇫',
+     code: 'AF',
+   },
+   AG: {
+     phone: 1268,
+     emoji: '🇦🇬',
+     code: 'AG',
+   },
+   AI: {
+     phone: 1264,
+     emoji: '🇦🇮',
+     code: 'AI',
+   },
+   AL: {
+     phone: 355,
+     emoji: '🇦🇱',
+     code: 'AL',
+   },
+   AM: {
+     phone: 374,
+     emoji: '🇦🇲',
+     code: 'AM',
+   },
+   AO: {
+     phone: 244,
+     emoji: '🇦🇴',
+     code: 'AO',
+   },
+   AQ: {
+     phone: 672,
+     emoji: '🇦🇶',
+     code: 'AQ',
+   },
+   AR: {
+     phone: 54,
+     emoji: '🇦🇷',
+     code: 'AR',
+   },
+   AS: {
+     phone: 1684,
+     emoji: '🇦🇸',
+     code: 'AS',
+   },
+   AT: {
+     phone: 43,
+     emoji: '🇦🇹',
+     code: 'AT',
+   },
+   AU: {
+     phone: 61,
+     emoji: '🇦🇺',
+     code: 'AU',
+   },
+   AW: {
+     phone: 297,
+     emoji: '🇦🇼',
+     code: 'AW',
+   },
+   AX: {
+     phone: 358,
+     emoji: '🇦🇽',
+     code: 'AX',
+   },
+   AZ: {
+     phone: 994,
+     emoji: '🇦🇿',
+     code: 'AZ',
+   },
+   BA: {
+     phone: 387,
+     emoji: '🇧🇦',
+     code: 'BA',
+   },
+   BB: {
+     phone: 1246,
+     emoji: '🇧🇧',
+     code: 'BB',
+   },
+   BD: {
+     phone: 880,
+     emoji: '🇧🇩',
+     code: 'BD',
+   },
+   BE: {
+     phone: 32,
+     emoji: '🇧🇪',
+     code: 'BE',
+   },
+   BF: {
+     phone: 226,
+     emoji: '🇧🇫',
+     code: 'BF',
+   },
+   BG: {
+     phone: 359,
+     emoji: '🇧🇬',
+     code: 'BG',
+   },
+   BH: {
+     phone: 973,
+     emoji: '🇧🇭',
+     code: 'BH',
+   },
+   BI: {
+     phone: 257,
+     emoji: '🇧🇮',
+     code: 'BI',
+   },
+   BJ: {
+     phone: 229,
+     emoji: '🇧🇯',
+     code: 'BJ',
+   },
+   BL: {
+     phone: 590,
+     emoji: '🇧🇱',
+     code: 'BL',
+   },
+   BM: {
+     phone: 1441,
+     emoji: '🇧🇲',
+     code: 'BM',
+   },
+   BN: {
+     phone: 673,
+     emoji: '🇧🇳',
+     code: 'BN',
+   },
+   BO: {
+     phone: 591,
+     emoji: '🇧🇴',
+     code: 'BO',
+   },
+   BQ: {
+     phone: 5997,
+     emoji: '🇧🇶',
+     code: 'BQ',
+   },
+   BR: {
+     phone: 55,
+     emoji: '🇧🇷',
+     code: 'BR',
+   },
+   BS: {
+     phone: 1242,
+     emoji: '🇧🇸',
+     code: 'BS',
+   },
+   BT: {
+     phone: 975,
+     emoji: '🇧🇹',
+     code: 'BT',
+   },
+   BV: {
+     phone: 47,
+     emoji: '🇧🇻',
+     code: 'BV',
+   },
+   BW: {
+     phone: 267,
+     emoji: '🇧🇼',
+     code: 'BW',
+   },
+   BY: {
+     phone: 375,
+     emoji: '🇧🇾',
+     code: 'BY',
+   },
+   BZ: {
+     phone: 501,
+     emoji: '🇧🇿',
+     code: 'BZ',
+   },
+   CA: {
+     phone: 1,
+     emoji: '🇨🇦',
+     code: 'CA',
+   },
+   CC: {
+     phone: 61,
+     emoji: '🇨🇨',
+     code: 'CC',
+   },
+   CD: {
+     phone: 243,
+     emoji: '🇨🇩',
+     code: 'CD',
+   },
+   CF: {
+     phone: 236,
+     emoji: '🇨🇫',
+     code: 'CF',
+   },
+   CG: {
+     phone: 242,
+     emoji: '🇨🇬',
+     code: 'CG',
+   },
+   CH: {
+     phone: 41,
+     emoji: '🇨🇭',
+     code: 'CH',
+   },
+   CI: {
+     phone: 225,
+     emoji: '🇨🇮',
+     code: 'CI',
+   },
+   CK: {
+     phone: 682,
+     emoji: '🇨🇰',
+     code: 'CK',
+   },
+   CL: {
+     phone: 56,
+     emoji: '🇨🇱',
+     code: 'CL',
+   },
+   CM: {
+     phone: 237,
+     emoji: '🇨🇲',
+     code: 'CM',
+   },
+   CN: {
+     phone: 86,
+     emoji: '🇨🇳',
+     code: 'CN',
+   },
+   CO: {
+     phone: 57,
+     emoji: '🇨🇴',
+     code: 'CO',
+   },
+   CR: {
+     phone: 506,
+     emoji: '🇨🇷',
+     code: 'CR',
+   },
+   CU: {
+     phone: 53,
+     emoji: '🇨🇺',
+     code: 'CU',
+   },
+   CV: {
+     phone: 238,
+     emoji: '🇨🇻',
+     code: 'CV',
+   },
+   CW: {
+     phone: 5999,
+     emoji: '🇨🇼',
+     code: 'CW',
+   },
+   CX: {
+     phone: 61,
+     emoji: '🇨🇽',
+     code: 'CX',
+   },
+   CY: {
+     phone: 357,
+     emoji: '🇨🇾',
+     code: 'CY',
+   },
+   CZ: {
+     phone: 420,
+     emoji: '🇨🇿',
+     code: 'CZ',
+   },
+   DE: {
+     phone: 49,
+     emoji: '🇩🇪',
+     code: 'DE',
+   },
+   DJ: {
+     phone: 253,
+     emoji: '🇩🇯',
+     code: 'DJ',
+   },
+   DK: {
+     phone: 45,
+     emoji: '🇩🇰',
+     code: 'DK',
+   },
+   DM: {
+     phone: 1767,
+     emoji: '🇩🇲',
+     code: 'DM',
+   },
+   DO: {
+     phone: 1809,
+     emoji: '🇩🇴',
+     code: 'DO',
+   },
+   DZ: {
+     phone: 213,
+     emoji: '🇩🇿',
+     code: 'DZ',
+   },
+   EC: {
+     phone: 593,
+     emoji: '🇪🇨',
+     code: 'EC',
+   },
+   EE: {
+     phone: 372,
+     emoji: '🇪🇪',
+     code: 'EE',
+   },
+   EG: {
+     phone: 20,
+     emoji: '🇪🇬',
+     code: 'EG',
+   },
+   EH: {
+     phone: 212,
+     emoji: '🇪🇭',
+     code: 'EH',
+   },
+   ER: {
+     phone: 291,
+     emoji: '🇪🇷',
+     code: 'ER',
+   },
+   ES: {
+     phone: 34,
+     emoji: '🇪🇸',
+     code: 'ES',
+   },
+   ET: {
+     phone: 251,
+     emoji: '🇪🇹',
+     code: 'ET',
+   },
+   FI: {
+     phone: 358,
+     emoji: '🇫🇮',
+     code: 'FI',
+   },
+   FJ: {
+     phone: 679,
+     emoji: '🇫🇯',
+     code: 'FJ',
+   },
+   FK: {
+     phone: 500,
+     emoji: '🇫🇰',
+     code: 'FK',
+   },
+   FM: {
+     phone: 691,
+     emoji: '🇫🇲',
+     code: 'FM',
+   },
+   FO: {
+     phone: 298,
+     emoji: '🇫🇴',
+     code: 'FO',
+   },
+   FR: {
+     phone: 33,
+     emoji: '🇫🇷',
+     code: 'FR',
+   },
+   GA: {
+     phone: 241,
+     emoji: '🇬🇦',
+     code: 'GA',
+   },
+   GB: {
+     phone: 44,
+     emoji: '🇬🇧',
+     code: 'GB',
+   },
+   GD: {
+     phone: 1473,
+     emoji: '🇬🇩',
+     code: 'GD',
+   },
+   GE: {
+     phone: 995,
+     emoji: '🇬🇪',
+     code: 'GE',
+   },
+   GF: {
+     phone: 594,
+     emoji: '🇬🇫',
+     code: 'GF',
+   },
+   GG: {
+     phone: 44,
+     emoji: '🇬🇬',
+     code: 'GG',
+   },
+   GH: {
+     phone: 233,
+     emoji: '🇬🇭',
+     code: 'GH',
+   },
+   GI: {
+     phone: 350,
+     emoji: '🇬🇮',
+     code: 'GI',
+   },
+   GL: {
+     phone: 299,
+     emoji: '🇬🇱',
+     code: 'GL',
+   },
+   GM: {
+     phone: 220,
+     emoji: '🇬🇲',
+     code: 'GM',
+   },
+   GN: {
+     phone: 224,
+     emoji: '🇬🇳',
+     code: 'GN',
+   },
+   GP: {
+     phone: 590,
+     emoji: '🇬🇵',
+     code: 'GP',
+   },
+   GQ: {
+     phone: 240,
+     emoji: '🇬🇶',
+     code: 'GQ',
+   },
+   GR: {
+     phone: 30,
+     emoji: '🇬🇷',
+     code: 'GR',
+   },
+   GS: {
+     phone: 500,
+     emoji: '🇬🇸',
+     code: 'GS',
+   },
+   GT: {
+     phone: 502,
+     emoji: '🇬🇹',
+     code: 'GT',
+   },
+   GU: {
+     phone: 1671,
+     emoji: '🇬🇺',
+     code: 'GU',
+   },
+   GW: {
+     phone: 245,
+     emoji: '🇬🇼',
+     code: 'GW',
+   },
+   GY: {
+     phone: 592,
+     emoji: '🇬🇾',
+     code: 'GY',
+   },
+   HK: {
+     phone: 852,
+     emoji: '🇭🇰',
+     code: 'HK',
+   },
+   HM: {
+     phone: 61,
+     emoji: '🇭🇲',
+     code: 'HM',
+   },
+   HN: {
+     phone: 504,
+     emoji: '🇭🇳',
+     code: 'HN',
+   },
+   HR: {
+     phone: 385,
+     emoji: '🇭🇷',
+     code: 'HR',
+   },
+   HT: {
+     phone: 509,
+     emoji: '🇭🇹',
+     code: 'HT',
+   },
+   HU: {
+     phone: 36,
+     emoji: '🇭🇺',
+     code: 'HU',
+   },
+   ID: {
+     phone: 62,
+     emoji: '🇮🇩',
+     code: 'ID',
+   },
+   IE: {
+     phone: 353,
+     emoji: '🇮🇪',
+     code: 'IE',
+   },
+   IL: {
+     phone: 972,
+     emoji: '🇮🇱',
+     code: 'IL',
+   },
+   IM: {
+     phone: 44,
+     emoji: '🇮🇲',
+     code: 'IM',
+   },
+   IN: {
+     phone: 91,
+     emoji: '🇮🇳',
+     code: 'IN',
+   },
+   IO: {
+     phone: 246,
+     emoji: '🇮🇴',
+     code: 'IO',
+   },
+   IQ: {
+     phone: 964,
+     emoji: '🇮🇶',
+     code: 'IQ',
+   },
+   IR: {
+     phone: 98,
+     emoji: '🇮🇷',
+     code: 'IR',
+   },
+   IS: {
+     phone: 354,
+     emoji: '🇮🇸',
+     code: 'IS',
+   },
+   IT: {
+     phone: 39,
+     emoji: '🇮🇹',
+     code: 'IT',
+   },
+   JE: {
+     phone: 44,
+     emoji: '🇯🇪',
+     code: 'JE',
+   },
+   JM: {
+     phone: 1876,
+     emoji: '🇯🇲',
+     code: 'JM',
+   },
+   JO: {
+     phone: 962,
+     emoji: '🇯🇴',
+     code: 'JO',
+   },
+   JP: {
+     phone: 81,
+     emoji: '🇯🇵',
+     code: 'JP',
+   },
+   KE: {
+     phone: 254,
+     emoji: '🇰🇪',
+     code: 'KE',
+   },
+   KG: {
+     phone: 996,
+     emoji: '🇰🇬',
+     code: 'KG',
+   },
+   KH: {
+     phone: 855,
+     emoji: '🇰🇭',
+     code: 'KH',
+   },
+   KI: {
+     phone: 686,
+     emoji: '🇰🇮',
+     code: 'KI',
+   },
+   KM: {
+     phone: 269,
+     emoji: '🇰🇲',
+     code: 'KM',
+   },
+   KN: {
+     phone: 1869,
+     emoji: '🇰🇳',
+     code: 'KN',
+   },
+   KP: {
+     phone: 850,
+     emoji: '🇰🇵',
+     code: 'KP',
+   },
+   KR: {
+     phone: 82,
+     emoji: '🇰🇷',
+     code: 'KR',
+   },
+   KW: {
+     phone: 965,
+     emoji: '🇰🇼',
+     code: 'KW',
+   },
+   KY: {
+     phone: 1345,
+     emoji: '🇰🇾',
+     code: 'KY',
+   },
+   KZ: {
+     phone: 76,
+     emoji: '🇰🇿',
+     code: 'KZ',
+   },
+   LA: {
+     phone: 856,
+     emoji: '🇱🇦',
+     code: 'LA',
+   },
+   LB: {
+     phone: 961,
+     emoji: '🇱🇧',
+     code: 'LB',
+   },
+   LC: {
+     phone: 1758,
+     emoji: '🇱🇨',
+     code: 'LC',
+   },
+   LI: {
+     phone: 423,
+     emoji: '🇱🇮',
+     code: 'LI',
+   },
+   LK: {
+     phone: 94,
+     emoji: '🇱🇰',
+     code: 'LK',
+   },
+   LR: {
+     phone: 231,
+     emoji: '🇱🇷',
+     code: 'LR',
+   },
+   LS: {
+     phone: 266,
+     emoji: '🇱🇸',
+     code: 'LS',
+   },
+   LT: {
+     phone: 370,
+     emoji: '🇱🇹',
+     code: 'LT',
+   },
+   LU: {
+     phone: 352,
+     emoji: '🇱🇺',
+     code: 'LU',
+   },
+   LV: {
+     phone: 371,
+     emoji: '🇱🇻',
+     code: 'LV',
+   },
+   LY: {
+     phone: 218,
+     emoji: '🇱🇾',
+     code: 'LY',
+   },
+   MA: {
+     phone: 212,
+     emoji: '🇲🇦',
+     code: 'MA',
+   },
+   MC: {
+     phone: 377,
+     emoji: '🇲🇨',
+     code: 'MC',
+   },
+   MD: {
+     phone: 373,
+     emoji: '🇲🇩',
+     code: 'MD',
+   },
+   ME: {
+     phone: 382,
+     emoji: '🇲🇪',
+     code: 'ME',
+   },
+   MF: {
+     phone: 590,
+     emoji: '🇲🇫',
+     code: 'MF',
+   },
+   MG: {
+     phone: 261,
+     emoji: '🇲🇬',
+     code: 'MG',
+   },
+   MH: {
+     phone: 692,
+     emoji: '🇲🇭',
+     code: 'MH',
+   },
+   MK: {
+     phone: 389,
+     emoji: '🇲🇰',
+     code: 'MK',
+   },
+   ML: {
+     phone: 223,
+     emoji: '🇲🇱',
+     code: 'ML',
+   },
+   MM: {
+     phone: 95,
+     emoji: '🇲🇲',
+     code: 'MM',
+   },
+   MN: {
+     phone: 976,
+     emoji: '🇲🇳',
+     code: 'MN',
+   },
+   MO: {
+     phone: 853,
+     emoji: '🇲🇴',
+     code: 'MO',
+   },
+   MP: {
+     phone: 1670,
+     emoji: '🇲🇵',
+     code: 'MP',
+   },
+   MQ: {
+     phone: 596,
+     emoji: '🇲🇶',
+     code: 'MQ',
+   },
+   MR: {
+     phone: 222,
+     emoji: '🇲🇷',
+     code: 'MR',
+   },
+   MS: {
+     phone: 1664,
+     emoji: '🇲🇸',
+     code: 'MS',
+   },
+   MT: {
+     phone: 356,
+     emoji: '🇲🇹',
+     code: 'MT',
+   },
+   MU: {
+     phone: 230,
+     emoji: '🇲🇺',
+     code: 'MU',
+   },
+   MV: {
+     phone: 960,
+     emoji: '🇲🇻',
+     code: 'MV',
+   },
+   MW: {
+     phone: 265,
+     emoji: '🇲🇼',
+     code: 'MW',
+   },
+   MX: {
+     phone: 52,
+     emoji: '🇲🇽',
+     code: 'MX',
+   },
+   MY: {
+     phone: 60,
+     emoji: '🇲🇾',
+     code: 'MY',
+   },
+   MZ: {
+     phone: 258,
+     emoji: '🇲🇿',
+     code: 'MZ',
+   },
+   NA: {
+     phone: 264,
+     emoji: '🇳🇦',
+     code: 'NA',
+   },
+   NC: {
+     phone: 687,
+     emoji: '🇳🇨',
+     code: 'NC',
+   },
+   NE: {
+     phone: 227,
+     emoji: '🇳🇪',
+     code: 'NE',
+   },
+   NF: {
+     phone: 672,
+     emoji: '🇳🇫',
+     code: 'NF',
+   },
+   NG: {
+     phone: 234,
+     emoji: '🇳🇬',
+     code: 'NG',
+   },
+   NI: {
+     phone: 505,
+     emoji: '🇳🇮',
+     code: 'NI',
+   },
+   NL: {
+     phone: 31,
+     emoji: '🇳🇱',
+     code: 'NL',
+   },
+   NO: {
+     phone: 47,
+     emoji: '🇳🇴',
+     code: 'NO',
+   },
+   NP: {
+     phone: 977,
+     emoji: '🇳🇵',
+     code: 'NP',
+   },
+   NR: {
+     phone: 674,
+     emoji: '🇳🇷',
+     code: 'NR',
+   },
+   NU: {
+     phone: 683,
+     emoji: '🇳🇺',
+     code: 'NU',
+   },
+   NZ: {
+     phone: 64,
+     emoji: '🇳🇿',
+     code: 'NZ',
+   },
+   OM: {
+     phone: 968,
+     emoji: '🇴🇲',
+     code: 'OM',
+   },
+   PA: {
+     phone: 507,
+     emoji: '🇵🇦',
+     code: 'PA',
+   },
+   PE: {
+     phone: 51,
+     emoji: '🇵🇪',
+     code: 'PE',
+   },
+   PF: {
+     phone: 689,
+     emoji: '🇵🇫',
+     code: 'PF',
+   },
+   PG: {
+     phone: 675,
+     emoji: '🇵🇬',
+     code: 'PG',
+   },
+   PH: {
+     phone: 63,
+     emoji: '🇵🇭',
+     code: 'PH',
+   },
+   PK: {
+     phone: 92,
+     emoji: '🇵🇰',
+     code: 'PK',
+   },
+   PL: {
+     phone: 48,
+     emoji: '🇵🇱',
+     code: 'PL',
+   },
+   PM: {
+     phone: 508,
+     emoji: '🇵🇲',
+     code: 'PM',
+   },
+   PN: {
+     phone: 64,
+     emoji: '🇵🇳',
+     code: 'PN',
+   },
+   PR: {
+     phone: 1787,
+     emoji: '🇵🇷',
+     code: 'PR',
+   },
+   PS: {
+     phone: 970,
+     emoji: '🇵🇸',
+     code: 'PS',
+   },
+   PT: {
+     phone: 351,
+     emoji: '🇵🇹',
+     code: 'PT',
+   },
+   PW: {
+     phone: 680,
+     emoji: '🇵🇼',
+     code: 'PW',
+   },
+   PY: {
+     phone: 595,
+     emoji: '🇵🇾',
+     code: 'PY',
+   },
+   QA: {
+     phone: 974,
+     emoji: '🇶🇦',
+     code: 'QA',
+   },
+   RE: {
+     phone: 262,
+     emoji: '🇷🇪',
+     code: 'RE',
+   },
+   RO: {
+     phone: 40,
+     emoji: '🇷🇴',
+     code: 'RO',
+   },
+   RS: {
+     phone: 381,
+     emoji: '🇷🇸',
+     code: 'RS',
+   },
+   RU: {
+     phone: 7,
+     emoji: '🇷🇺',
+     code: 'RU',
+   },
+   RW: {
+     phone: 250,
+     emoji: '🇷🇼',
+     code: 'RW',
+   },
+   SA: {
+     phone: 966,
+     emoji: '🇸🇦',
+     code: 'SA',
+   },
+   SB: {
+     phone: 677,
+     emoji: '🇸🇧',
+     code: 'SB',
+   },
+   SC: {
+     phone: 248,
+     emoji: '🇸🇨',
+     code: 'SC',
+   },
+   SD: {
+     phone: 249,
+     emoji: '🇸🇩',
+     code: 'SD',
+   },
+   SE: {
+     phone: 46,
+     emoji: '🇸🇪',
+     code: 'SE',
+   },
+   SG: {
+     phone: 65,
+     emoji: '🇸🇬',
+     code: 'SG',
+   },
+   SH: {
+     phone: 290,
+     emoji: '🇸🇭',
+     code: 'SH',
+   },
+   SI: {
+     phone: 386,
+     emoji: '🇸🇮',
+     code: 'SI',
+   },
+   SJ: {
+     phone: 4779,
+     emoji: '🇸🇯',
+     code: 'SJ',
+   },
+   SK: {
+     phone: 421,
+     emoji: '🇸🇰',
+     code: 'SK',
+   },
+   SL: {
+     phone: 232,
+     emoji: '🇸🇱',
+     code: 'SL',
+   },
+   SM: {
+     phone: 378,
+     emoji: '🇸🇲',
+     code: 'SM',
+   },
+   SN: {
+     phone: 221,
+     emoji: '🇸🇳',
+     code: 'SN',
+   },
+   SO: {
+     phone: 252,
+     emoji: '🇸🇴',
+     code: 'SO',
+   },
+   SR: {
+     phone: 597,
+     emoji: '🇸🇷',
+     code: 'SR',
+   },
+   SS: {
+     phone: 211,
+     emoji: '🇸🇸',
+     code: 'SS',
+   },
+   ST: {
+     phone: 239,
+     emoji: '🇸🇹',
+     code: 'ST',
+   },
+   SV: {
+     phone: 503,
+     emoji: '🇸🇻',
+     code: 'SV',
+   },
+   SX: {
+     phone: 1721,
+     emoji: '🇸🇽',
+     code: 'SX',
+   },
+   SY: {
+     phone: 963,
+     emoji: '🇸🇾',
+     code: 'SY',
+   },
+   SZ: {
+     phone: 268,
+     emoji: '🇸🇿',
+     code: 'SZ',
+   },
+   TC: {
+     phone: 1649,
+     emoji: '🇹🇨',
+     code: 'TC',
+   },
+   TD: {
+     phone: 235,
+     emoji: '🇹🇩',
+     code: 'TD',
+   },
+   TF: {
+     phone: 262,
+     emoji: '🇹🇫',
+     code: 'TF',
+   },
+   TG: {
+     phone: 228,
+     emoji: '🇹🇬',
+     code: 'TG',
+   },
+   TH: {
+     phone: 66,
+     emoji: '🇹🇭',
+     code: 'TH',
+   },
+   TJ: {
+     phone: 992,
+     emoji: '🇹🇯',
+     code: 'TJ',
+   },
+   TK: {
+     phone: 690,
+     emoji: '🇹🇰',
+     code: 'TK',
+   },
+   TL: {
+     phone: 670,
+     emoji: '🇹🇱',
+     code: 'TL',
+   },
+   TM: {
+     phone: 993,
+     emoji: '🇹🇲',
+     code: 'TM',
+   },
+   TN: {
+     phone: 216,
+     emoji: '🇹🇳',
+     code: 'TN',
+   },
+   TO: {
+     phone: 676,
+     emoji: '🇹🇴',
+     code: 'TO',
+   },
+   TR: {
+     phone: 90,
+     emoji: '🇹🇷',
+     code: 'TR',
+   },
+   TT: {
+     phone: 1868,
+     emoji: '🇹🇹',
+     code: 'TT',
+   },
+   TV: {
+     phone: 688,
+     emoji: '🇹🇻',
+     code: 'TV',
+   },
+   TW: {
+     phone: 886,
+     emoji: '🇹🇼',
+     code: 'TW',
+   },
+   TZ: {
+     phone: 255,
+     emoji: '🇹🇿',
+     code: 'TZ',
+   },
+   UA: {
+     phone: 380,
+     emoji: '🇺🇦',
+     code: 'UA',
+   },
+   UG: {
+     phone: 256,
+     emoji: '🇺🇬',
+     code: 'UG',
+   },
+   UM: {
+     phone: 1,
+     emoji: '🇺🇲',
+     code: 'UM',
+   },
+   US: {
+     phone: 1,
+     emoji: '🇺🇸',
+     code: 'US',
+   },
+   UY: {
+     phone: 598,
+     emoji: '🇺🇾',
+     code: 'UY',
+   },
+   UZ: {
+     phone: 998,
+     emoji: '🇺🇿',
+     code: 'UZ',
+   },
+   VA: {
+     phone: 379,
+     emoji: '🇻🇦',
+     code: 'VA',
+   },
+   VC: {
+     phone: 1784,
+     emoji: '🇻🇨',
+     code: 'VC',
+   },
+   VE: {
+     phone: 58,
+     emoji: '🇻🇪',
+     code: 'VE',
+   },
+   VG: {
+     phone: 1284,
+     emoji: '🇻🇬',
+     code: 'VG',
+   },
+   VI: {
+     phone: 1340,
+     emoji: '🇻🇮',
+     code: 'VI',
+   },
+   VN: {
+     phone: 84,
+     emoji: '🇻🇳',
+     code: 'VN',
+   },
+   VU: {
+     phone: 678,
+     emoji: '🇻🇺',
+     code: 'VU',
+   },
+   WF: {
+     phone: 681,
+     emoji: '🇼🇫',
+     code: 'WF',
+   },
+   WS: {
+     phone: 685,
+     emoji: '🇼🇸',
+     code: 'WS',
+   },
+   XK: {
+     phone: 377,
+     emoji: '🇽🇰',
+     code: 'XK',
+   },
+   YE: {
+     phone: 967,
+     emoji: '🇾🇪',
+     code: 'YE',
+   },
+   YT: {
+     phone: 262,
+     emoji: '🇾🇹',
+     code: 'YT',
+   },
+   ZA: {
+     phone: 27,
+     emoji: '🇿🇦',
+     code: 'ZA',
+   },
+   ZM: {
+     phone: 260,
+     emoji: '🇿🇲',
+     code: 'ZM',
+   },
+   ZW: {
+     phone: 263,
+     emoji: '🇿🇼',
+     code: 'ZW',
+   },
+ };
+ 
---
+++ index.js
@@ -0,0 +1,63 @@
+const _ = require('lodash');
+const updateNotifier = require('update-notifier');
+const PNF = require('google-libphonenumber').PhoneNumberFormat;
+const phoneUtil = require('google-libphonenumber').PhoneNumberUtil.getInstance();
+
+const countryCodes = require('./countryCodes.js');
+
+const pkg = require('./package.json');
+updateNotifier({pkg}).notify();
+
+module.exports = {
+    countryCodeList: () => {
+        try {
+            const sortedListByPhone = _.sortBy(countryCodes, ['phone']);
+            const output = _.map(sortedListByPhone, ({ phone, emoji, code }, key) => ({
+                phone,
+                emoji,
+                code,
+                key
+            }))
+
+            return output;
+            
+        } catch (e) {
+            return { phone: '', emoji: '', code: '', key: ''}
+        }
+    },
+
+    countryCodeFilteredList: (filterKey) => {
+        try {
+            const key = parseInt(filterKey);
+            if (isNaN(key)) {
+                const upperCaseKey = filterKey.toUpperCase();
+                return _.find(countryCodeList(), ['code', upperCaseKey]);
+            }
+    
+            return _.find(countryCodeList(), ['phone', key]);
+
+        } catch (e) {
+            return {};
+        }
+    },
+
+    phoneNumberValidation: (number, countryCode) => {
+        try {
+            const country = countryCodeFilteredList(countryCode)
+            const phoneNumber = phoneUtil.parse(number.split('+').join(''), country['code']);
+
+            return {
+                phone : phoneUtil.format(phoneNumber, PNF.E164),
+                isValid : phoneUtil.isValidNumber(phoneNumber),
+                success: true,
+                code: country['code'],
+                flag: country['emoji']
+              }
+
+        } catch (e) {
+            return {
+                success: false,
+            }
+        }
+    }
+}
